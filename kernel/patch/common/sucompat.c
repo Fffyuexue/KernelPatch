6dc5c18f03b8e36ae460646544099ea2b3dcdaef
makefilemememe <3320002755@qq.com>
2025-12-27 03:03:53 +0800

    Update sucompat.c

--- kernel/patch/common/sucompat.c
+++ kernel/patch/common/sucompat.c
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /* 
  * Copyright (C) 2023 bmax121. All Rights Reserved.
+ * 
+ * MODIFIED: Removed faccessat/fstatat hooks (su_handler_arg1_ufilename_before).
+ * RATIONALE: To eliminate "Detected delayed syscall" by allowing file access checks
+ * to run at native kernel speed. Hiding logic is delegated to userspace modules.
  */
 
 #include <linux/list.h>
@@ -77,7 +81,6 @@ int su_add_allow_uid(uid_t uid, uid_t to_uid, const char *scontext)
     };
     memcpy(profile.scontext, scontext, SUPERCALL_SCONTEXT_LEN);
     int rc = write_kstorage(su_kstorage_gid, uid, &profile, 0, sizeof(struct su_profile), false);
-    logkfd("uid: %d, to_uid: %d, sctx: %s, rc: %d\n", uid, to_uid, scontext, rc);
     return rc;
 }
 KP_EXPORT_SYMBOL(su_add_allow_uid);
@@ -113,7 +116,6 @@ static int allow_uids_cb(struct kstorage *kstorage, void *udata)
     if (up->is_user) {
         int cprc = compat_copy_to_user(up->out_uids + up->idx, &profile->uid, sizeof(uid_t));
         if (cprc <= 0) {
-            logkfd("compat_copy_to_user error: %d", cprc);
             return cprc;
         }
     } else {
@@ -156,7 +158,6 @@ int su_allow_uid_profile(int is_user, uid_t uid, struct su_profile *out_profile)
     if (is_user) {
         rc = compat_copy_to_user(out_profile, profile, sizeof(struct su_profile));
         if (rc <= 0) {
-            logkfd("compat_copy_to_user error: %d", rc);
             goto out;
         }
     } else {
@@ -175,7 +176,6 @@ int su_reset_path(const char *path)
     if (!path) return -EINVAL;
     if (IS_ERR(path)) return PTR_ERR(path);
     current_su_path = path;
-    logkfd("%s\n", current_su_path);
     dsb(ish);
     return 0;
 }
@@ -212,7 +212,6 @@ static void handle_before_execve(char **__user u_filename_p, char **__user uargv
             if (uptr && !IS_ERR(uptr)) {
                 *u_filename_p = (char *__user)uptr;
             }
-            logkfi("call su uid: %d, to_uid: %d, sctx: %s, uptr: %llx\n", uid, to_uid, sctx, uptr);
 #ifdef ANDROID
         } else {
             filp_close(filp, 0);
@@ -236,15 +235,10 @@ static void handle_before_execve(char **__user u_filename_p, char **__user uargv
                     sp &= 0xFFFFFFFFFFFFFFF8;
                     argv_cplen = compat_copy_to_user((void *)sp, legacy_su_path, sizeof(legacy_su_path));
                     if (argv_cplen > 0) {
-                        int rc = set_user_arg_ptr(0, *uargv, 0, sp);
-                        if (rc < 0) { // todo: modify entire argv
-                            logkfi("call apd argv error, uid: %d, to_uid: %d, sctx: %s, rc: %d\n", uid, to_uid, sctx,
-                                   rc);
-                        }
+                        set_user_arg_ptr(0, *uargv, 0, sp);
                     }
                 }
             }
-            logkfi("call apd uid: %d, to_uid: %d, sctx: %s, cplen: %d, %d\n", uid, to_uid, sctx, cplen, argv_cplen);
         }
 #endif // ANDROID
     } else if (!strcmp(SUPERCMD, filename)) {
@@ -254,15 +248,6 @@ static void handle_before_execve(char **__user u_filename_p, char **__user uargv
     }
 }
 
-// https://elixir.bootlin.com/linux/v6.1/source/fs/exec.c#L2107
-// COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
-// 	const compat_uptr_t __user *, argv,
-// 	const compat_uptr_t __user *, envp)
-
-// https://elixir.bootlin.com/linux/v6.1/source/fs/exec.c#L2087
-// SYSCALL_DEFINE3(execve, const char __user *, filename, const char __user *const __user *, argv,
-//                 const char __user *const __user *, envp)
-
 static void before_execve(hook_fargs3_t *args, void *udata)
 {
     void *arg0p = syscall_argn_p(args, 0);
@@ -270,16 +255,6 @@ static void before_execve(hook_fargs3_t *args, void *udata)
     handle_before_execve((char **)arg0p, (char **)arg1p, udata);
 }
 
-// https://elixir.bootlin.com/linux/v6.1/source/fs/exec.c#L2114
-// COMPAT_SYSCALL_DEFINE5(execveat, int, fd,
-// 		       const char __user *, filename,
-// 		       const compat_uptr_t __user *, argv,
-// 		       const compat_uptr_t __user *, envp,
-// 		       int,  flags)
-
-// https://elixir.bootlin.com/linux/v6.1/source/fs/exec.c#L2095
-// SYSCALL_DEFINE5(execveat, int, fd, const char __user *, filename, const char __user *const __user *, argv,
-//                 const char __user *const __user *, envp, int, flags)
 __maybe_unused static void before_execveat(hook_fargs5_t *args, void *udata)
 {
     void *arg1p = syscall_argn_p(args, 1);
@@ -287,41 +262,9 @@ __maybe_unused static void before_execveat(hook_fargs5_t *args, void *udata)
     handle_before_execve((char **)arg1p, (char **)arg2p, udata);
 }
 
-// https://elixir.bootlin.com/linux/v6.1/source/fs/stat.c#L431
-// SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
-// 		struct stat __user *, statbuf, int, flag)
-
-// https://elixir.bootlin.com/linux/v6.1/source/fs/open.c#L492
-// SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
-
-// https://elixir.bootlin.com/linux/v6.1/source/fs/open.c#L497
-// SYSCALL_DEFINE4(faccessat2, int, dfd, const char __user *, filename, int, mode, int, flags)
-
-// https://elixir.bootlin.com/linux/v6.1/source/fs/stat.c#L661
-// SYSCALL_DEFINE5(statx,
-// 		int, dfd, const char __user *, filename, unsigned, flags,
-// 		unsigned int, mask,
-// 		struct statx __user *, buffer)
-static void su_handler_arg1_ufilename_before(hook_fargs6_t *args, void *udata)
-{
-    uid_t uid = current_uid();
-    if (!is_su_allow_uid(uid)) return;
-
-    char __user **u_filename_p = (char __user **)syscall_argn_p(args, 1);
-
-    char filename[SU_PATH_MAX_LEN];
-    int flen = compat_strncpy_from_user(filename, *u_filename_p, sizeof(filename));
-    if (flen <= 0) return;
-
-    if (!strcmp(current_su_path, filename)) {
-        void *uptr = copy_to_user_stack(sh_path, sizeof(sh_path));
-        if (uptr && !IS_ERR(uptr)) {
-            *u_filename_p = uptr;
-        } else {
-            logkfi("su uid: %d, cp stack error: %d\n", uid, uptr);
-        }
-    }
-}
+// REMOVED FUNCTION: su_handler_arg1_ufilename_before
+// This function was responsible for hooking faccessat/fstatat and manipulating the filename.
+// It has been removed to eliminate syscall delays.
 
 int set_ap_mod_exclude(uid_t uid, int exclude)
 {
@@ -378,31 +321,26 @@ int su_compat_init()
 
     uint8_t su_config = patch_config->patch_su_config;
     bool enable = !!(su_config & PATCH_CONFIG_SU_ENABLE);
-    bool wrap = !!(su_config & PATCH_CONFIG_SU_HOOK_NO_WRAP);
-    log_boot("su config: %x, enable: %d, wrap: %d\n", su_config, enable, wrap);
-
+    // bool wrap = !!(su_config & PATCH_CONFIG_SU_HOOK_NO_WRAP);
+    
     // if (!enable) return;
 
+    // HOOKS FOR EXECVE (KEPT)
+    // These are essential for 'su' command functionality.
     rc = hook_syscalln(__NR_execve, 3, before_execve, 0, (void *)0);
-    log_boot("hook __NR_execve rc: %d\n", rc);
 
-    rc = hook_syscalln(__NR3264_fstatat, 4, su_handler_arg1_ufilename_before, 0, (void *)0);
-    log_boot("hook __NR3264_fstatat rc: %d\n", rc);
-
-    rc = hook_syscalln(__NR_faccessat, 3, su_handler_arg1_ufilename_before, 0, (void *)0);
-    log_boot("hook __NR_faccessat rc: %d\n", rc);
-
-    // __NR_execve 11
+    // COMPAT EXECVE (KEPT)
     rc = hook_compat_syscalln(11, 3, before_execve, 0, (void *)1);
-    log_boot("hook 32 __NR_execve rc: %d\n", rc);
 
-    // __NR_fstatat64 327
-    rc = hook_compat_syscalln(327, 4, su_handler_arg1_ufilename_before, 0, (void *)0);
-    log_boot("hook 32 __NR_fstatat64 rc: %d\n", rc);
-
-    //  __NR_faccessat 334
-    rc = hook_compat_syscalln(334, 3, su_handler_arg1_ufilename_before, 0, (void *)0);
-    log_boot("hook 32 __NR_faccessat rc: %d\n", rc);
+    // HOOKS FOR FACCESSAT/FSTATAT (REMOVED)
+    // To fix "Detected delayed syscall", we no longer hook these syscalls in the kernel.
+    // File hiding is now delegated to userspace modules or relies on the absence of physical files.
+    //
+    // rc = hook_syscalln(__NR3264_fstatat, 4, su_handler_arg1_ufilename_before, 0, (void *)0);
+    // rc = hook_syscalln(__NR_faccessat, 3, su_handler_arg1_ufilename_before, 0, (void *)0);
+    // rc = hook_compat_syscalln(327, 4, su_handler_arg1_ufilename_before, 0, (void *)0);
+    // rc = hook_compat_syscalln(334, 3, su_handler_arg1_ufilename_before, 0, (void *)0);
 
     return 0;
 }
+
